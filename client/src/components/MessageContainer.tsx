3c81eeafe8617af1a538a17cddf31628c3ac4a28
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-05-04 13:50:34 +0200

    added some stying to messagecontainer

--- client/src/components/MessageContainer.tsx
+++ client/src/components/MessageContainer.tsx
@@ -1,30 +1,36 @@
-import Form from './Form'
-import { io, Socket } from 'socket.io-client'
-import { ServerToClientEvents, ClientToServerEvents } from '../../../types'
-import { useEffect, useRef, useState } from 'react'
+import Form from "./Form";
+import { io, Socket } from "socket.io-client";
+import { ServerToClientEvents, ClientToServerEvents } from "../../../types";
+import { useEffect, useRef, useState } from "react";
+import { Box, Heading } from "@chakra-ui/react";
 
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io()
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
 function MessageContainer() {
-  const [Messages, setMessages] = useState<string[]>([])
-  const firstRun = useRef(true)
+  const [Messages, setMessages] = useState<string[]>([]);
+  const firstRun = useRef(true);
 
   useEffect(() => {
-    socket.on('chat message', (message) => {
-      setMessages([...Messages, message])
-    })
-  }, [Messages])
+    socket.on("chat message", (message) => {
+      setMessages([...Messages, message]);
+    });
+  }, [Messages]);
 
   return (
-    <>
-      <ul id="messages">
-        {Messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
-      <Form />
-    </>
-  )
+    <Box height="100%" position="relative">
+      <Box>
+        <ul id="messages">
+          <Heading textAlign='center'>All messages in *chatroomname*</Heading>
+          {Messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </Box>
+      <Box position="absolute" bottom={0} w='100%'>
+        <Form />
+      </Box>
+    </Box>
+  );
 }
 
-export default MessageContainer
+export default MessageContainer;
