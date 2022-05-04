3db1c082a80ef601e48c3a384727bcac11d5af81
Olivia isberg <oliviaisberg@gmail.com>
2022-05-04 14:47:23 +0200

    Passsing props

--- client/src/components/RoomList.tsx
+++ client/src/components/RoomList.tsx
@@ -1,9 +1,38 @@
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  Button,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import Form from './Form';
+import { useState } from 'react';
 function RoomList() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [creatingRoom, setCreatingRoom] = useState(true);
   return (
     <>
+      <Button onClick={onOpen} rightIcon={<AddCircleOutlineRoundedIcon />}>
+        Create Room
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Name your room</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Form {...{ setCreatingRoom }} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
       <h1>Roomlist</h1>
     </>
-  )
+  );
 }
 
-export default RoomList
+export default RoomList;
