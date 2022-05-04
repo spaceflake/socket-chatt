217e0f3769223d47193c74212367d63c90e70750
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-04 09:58:09 +0200

    created comps

---
+++ client/src/components/ActiveList.tsx
@@ -0,0 +1,9 @@
+function ActiveList() {
+  return (
+    <>
+      <h1>Active user list</h1>
+    </>
+  )
+}
+
+export default ActiveList
---
+++ client/src/components/RoomList.tsx
@@ -0,0 +1,9 @@
+function RoomList() {
+  return (
+    <>
+      <h1>Roomlist</h1>
+    </>
+  )
+}
+
+export default RoomList
