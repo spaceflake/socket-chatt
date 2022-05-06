8f0050482ece89b42fbb10a79b6ce4d18e909307
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-05-06 09:17:30 +0200

    commit

--- server/chatHandler.ts
+++ server/chatHandler.ts
@@ -15,6 +15,11 @@ export default (io: Server, socket: Socket) => {
     socket.emit('joined', room)
   })
 
+  socket.on('leave', (room) => {
+    socket.leave(room);
+    io.to(room).emit(`user has left the room`);
+  })
+
   socket.on('message', (message, to) => {
     console.log(message, to)
 
