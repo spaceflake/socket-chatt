aced4f8b905e8759b10ff3c151796e95a9bf081d
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-10 11:37:24 +0200

    Map start

--- server/roomMessageStore.ts
+++ server/roomMessageStore.ts
@@ -1,1 +1,7 @@
 // Map room objects with users and messages
+const roomMap = new Map();
+
+roomMap.set('room1', { id: 1, msg: ['hej', 'hej du'] });
+roomMap.set('room2', { id: 2, msg: ['bla', 'blabla'] });
+
+console.log(roomMap.get('room1'));
