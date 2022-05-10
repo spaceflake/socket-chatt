f885be034e0624e9d8d46eb945783d8bcd4ef46c
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-10 09:37:40 +0200

    Lista alla som är online Fixes #16

--- client/src/components/ActiveList.tsx
+++ client/src/components/ActiveList.tsx
@@ -1,40 +1,61 @@
-import { Box, Divider, Text } from "@chakra-ui/react"
+import { Box, Divider, Text } from '@chakra-ui/react';
 import MessageIcon from '@mui/icons-material/Message';
-import { useContext } from "react";
+import { useContext, useEffect } from 'react';
 import { SocketContext } from '../context/socketContext';
 
 function ActiveList() {
-  const { socket, nickname, allRooms, joinedRoom } =
-    useContext(SocketContext);
+  const { socket, nickname, allRooms, joinedRoom } = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on('userList', (users) => {
+      console.log(users);
+    });
+  }, [socket]);
+
+  // console.log(socket.on('userList', (users)));
+
+  // const users = socket.on('userList', (users)) => {
+  //   console.log(users)
+  // }
   return (
     <>
       {!joinedRoom ? (
         <></>
-      )
-        :
-        (
-          <>
-            <Text>
-              All online in this room:
-            </Text>
-            <Divider mt={5} mb={5} />
-            <Box className="scrollBox" h="100%">
-              {/* Do the loop of all users in a room */}
-              <ul>
-                <li><Text font-size="1rem">Morran <MessageIcon /></Text></li>
-                <Divider />
-                <li><Text font-size="1rem">Emma <MessageIcon /></Text></li>
-                <Divider />
-                <li><Text font-size="1rem">Julia <MessageIcon /></Text></li>
-                <Divider />
-                <li><Text font-size="1rem">Malin <MessageIcon /></Text></li>
-                <Divider />
-              </ul>
-            </Box>
-          </>
-        )
-      }
-
+      ) : (
+        <>
+          <Text>All online in this room:</Text>
+          <Divider mt={5} mb={5} />
+          <Box className="scrollBox" h="100%">
+            {/* Do the loop of all users in a room */}
+            <ul>
+              <li>
+                <Text font-size="1rem">
+                  Morran <MessageIcon />
+                </Text>
+              </li>
+              <Divider />
+              <li>
+                <Text font-size="1rem">
+                  Emma <MessageIcon />
+                </Text>
+              </li>
+              <Divider />
+              <li>
+                <Text font-size="1rem">
+                  Julia <MessageIcon />
+                </Text>
+              </li>
+              <Divider />
+              <li>
+                <Text font-size="1rem">
+                  Malin <MessageIcon />
+                </Text>
+              </li>
+              <Divider />
+            </ul>
+          </Box>
+        </>
+      )}
     </>
   );
 }
