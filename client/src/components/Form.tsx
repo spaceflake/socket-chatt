795195bc03e3371b15f646a872fb84c4f7266a17
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-05-10 11:16:18 +0200

    fixing chatform component, doesn't work yet.

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,12 +1,5 @@
-import {
-  Dispatch,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import SendIcon from '@mui/icons-material/Send';
+import { Dispatch, SetStateAction, useContext } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import {
   Button,
   FormControl,
@@ -14,39 +7,22 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-} from '@chakra-ui/react';
-import { SocketContext } from '../context/socketContext';
+} from "@chakra-ui/react";
+import { SocketContext } from "../context/socketContext";
 
 interface formProps {
   setIsOnline?: Dispatch<SetStateAction<boolean>>;
   setCreatingRoom?: Dispatch<SetStateAction<boolean>>;
-  setWritingMessage?: Dispatch<SetStateAction<boolean>>;
-  writingMessage?: boolean;
   creatingRoom?: boolean;
-  message?: string;
-  setMessage?: Dispatch<SetStateAction<string>>;
 }
 type Inputs = {
   input: string;
 };
 
-function Form({
-  setIsOnline,
-  setCreatingRoom,
-  setWritingMessage,
-  writingMessage,
-  creatingRoom,
-  message,
-  setMessage,
-}: formProps) {
+function Form({ setIsOnline, setCreatingRoom, creatingRoom }: formProps) {
   const { register, handleSubmit, watch, reset } = useForm<Inputs>();
-  const { socket, nickname, allRooms, joinedRoom } = useContext(SocketContext);
 
-  if (setWritingMessage) {
-    if (watch('input')) {
-      setWritingMessage(true);
-    }
-  }
+  const { socket, nickname, allRooms, joinedRoom } = useContext(SocketContext);
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     if (socket && !creatingRoom) {
@@ -57,29 +33,12 @@ function Form({
       socket.connect();
       let room = data.input;
       if (!room.length) {
-        console.log('Ogiltigt namn på rum...');
+        console.log("Ogiltigt namn på rum...");
         return;
       }
-      socket.emit('join', room);
+      socket.emit("join", room);
 
       setCreatingRoom(false);
-    }
-
-    if (writingMessage && setWritingMessage && message && setMessage) {
-      setMessage(data.input);
-
-      // let message = data.input;
-      if (!message.length) {
-        console.log('För kort meddelande');
-        return;
-      }
-
-      socket.emit('message', message, joinedRoom);
-      setWritingMessage(false);
-      setMessage('');
-
-      reset();
-      console.log('3', data);
     }
 
     setIsOnline && setIsOnline(true);
@@ -89,31 +48,27 @@ function Form({
       <FormControl>
         <FormLabel htmlFor="input">
           {setIsOnline
-            ? 'Enter your nickname, stupid!'
+            ? "Enter your nickname, stupid!"
             : setCreatingRoom
-            ? 'Enter roomname'
-            : setWritingMessage && ''}
+            ? "Enter roomname"
+            : ""}
         </FormLabel>
         <InputGroup>
           <Input
             type="text"
             id="input"
             autoComplete="off"
-            {...register('input')}
+            {...register("input")}
           />
           <InputRightElement w="fit-content" bg="rgba(255, 255, 255, 0.3)">
             <Button type="submit" variant="ghost">
-              {!creatingRoom ? 'Send' : 'Create Room'}
+              {!creatingRoom ? "Send" : "Create Room"}
             </Button>
           </InputRightElement>
         </InputGroup>
       </FormControl>
-      <Button type="submit" w="full" variant="solid" disabled={!watch('input')}>
-        {creatingRoom
-          ? 'Create Room'
-          : setIsOnline
-          ? 'Enter'
-          : setWritingMessage && <SendIcon />}
+      <Button type="submit" w="full" variant="solid" disabled={!watch("input")}>
+        {creatingRoom ? "Create Room" : setIsOnline ? "Enter" : ""}
       </Button>
     </form>
   );
