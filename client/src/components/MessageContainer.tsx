ae8ae062e0f9f212607f6283a736fd234fd95772
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-05-11 12:56:11 +0200

    moved scrollbox class

--- client/src/components/MessageContainer.tsx
+++ client/src/components/MessageContainer.tsx
@@ -1,6 +1,6 @@
-import Form from './Form';
-import { useContext, useEffect, useState } from 'react';
-import { Message } from '../../../types';
+import Form from "./Form";
+import { useContext, useEffect, useState } from "react";
+import { Message } from "../../../types";
 
 import {
   Box,
@@ -16,15 +16,15 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
-import { SocketContext } from '../context/socketContext';
-import communication from '../assets/com.png';
-import ChatForm from './ChatForm';
+import { SocketContext } from "../context/socketContext";
+import communication from "../assets/com.png";
+import ChatForm from "./ChatForm";
 
 function MessageContainer() {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [writingMessage, setWritingMessage] = useState(false);
   const { socket, nickname, allRooms, joinedRoom, chatMessages } =
     useContext(SocketContext);
@@ -38,31 +38,26 @@ function MessageContainer() {
   }, [creatingRoom]);
 
   useEffect(() => {
-    console.log('switched room to: ' + joinedRoom);
+    console.log("switched room to: " + joinedRoom);
   }, [joinedRoom]);
 
   useEffect(() => {
     // Receive room chat history
-    socket.on('history', (messages) => {
+    socket.on("history", (messages) => {
       setMessages(messages);
     });
 
-    socket.on('isWriting', (isWriting) => {
+    socket.on("isWriting", (isWriting) => {
       setWritingMessage(isWriting);
     });
 
-    socket.on('message', (message) => {
+    socket.on("message", (message) => {
       setMessages((messages) => [...messages, message]);
     });
   }, [socket]);
 
   return (
-    <Box
-      h="100%"
-      className="scrollBox"
-      position="relative"
-      bg="rgba(255,255,255, 0.5)"
-    >
+    <Box h="100%" position="relative" bg="rgba(255,255,255, 0.5)">
       {!joinedRoom ? (
         <Flex
           direction="column"
@@ -97,39 +92,40 @@ function MessageContainer() {
           </Modal>
         </Flex>
       ) : (
-        <>
-          <Box>
+        <Box h="100%">
+          <Box h="100%">
             <Heading textAlign="center">All messages in {joinedRoom}</Heading>
-            {chatMessages && (
-              <ul id="messages">
-                {messages.map((chatMessage, index) => (
-                  <Box
-                    key={index}
-                    bg="#739099"
-                    mt="1"
-                    p="1.5"
-                    w="fit-content"
-                    borderRadius="md"
-                  >
-                    <Text bg="blackAlpha.500" color="gray.100">
-                      From: {chatMessage.sender}
-                    </Text>
-                    <Text>{chatMessage.body}</Text>
-                  </Box>
-                ))}
-              </ul>
-            )}
+            <Box h="100%" className="scrollBox">
+              {chatMessages && (
+                <ul id="messages">
+                  {messages.map((chatMessage, index) => (
+                    <Box
+                      key={index}
+                      bg="#739099"
+                      mt="1"
+                      p="1.5"
+                      w="fit-content"
+                      borderRadius="md"
+                    >
+                      <Text bg="blackAlpha.500" color="gray.100">
+                        From: {chatMessage.sender}
+                      </Text>
+                      <Text>{chatMessage.body}</Text>
+                    </Box>
+                  ))}
+                </ul>
+              )}
+            </Box>
           </Box>
           <Box position="absolute" bottom={0} w="100%">
-            {/* add spinner thingy */}
-            <Text></Text>
-            <Text>{writingMessage && 'is writing'}</Text>
+            
+            <Text>{writingMessage && "someone is writing a message..."}</Text>
 
             <ChatForm
               {...{ setWritingMessage, writingMessage, message, setMessage }}
             />
           </Box>
-        </>
+        </Box>
       )}
     </Box>
   );
